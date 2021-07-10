8316ba0f2a6efca777daa6b954204d76aebd8088
Kyle Stewart <kylestew@gmail.com>
2021-07-10 12:17:59 -0600

    Basic code sample up and running - LED blink on board

---
+++ README.md
@@ -0,0 +1,28 @@
+# UXN on STM32 ARM
+
+## Getting Started with Development
+
+I used [this guide](https://rhye.org/post/stm32-with-opencm3-0-compiling-and-uploading/) to get started.
+
+### Building
+
+    $ make
+
+### Programming
+
+    $ openocd -f interface/stlink-v2.cfg -f target/stm32f4x.cfg -c "program main.elf verify reset exit"
+
+### Debugging
+
+???
+
+## Useful Docs
+
+Some important data sheets are included in the repo, but here is some further reading...
+
+[libopenCM3 examples for
+F4-Disco](https://github.com/libopencm3/libopencm3-examples/tree/master/examples/stm32/f4/stm32f4-discovery)
+
+[Example Makefile for libopenCM3](https://github.com/libopencm3/libopencm3-examples/blob/master/examples/rules.mk)
+
+[RM0090 STM32 Ref Manual](https://www.st.com/resource/en/reference_manual/dm00031020-stm32f405-415-stm32f407-417-stm32f427-437-and-stm32f429-439-advanced-arm-based-32-bit-mcus-stmicroelectronics.pdf)
---
+++ main.c
@@ -0,0 +1,42 @@
+#include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/rcc.h>
+
+static void gpio_setup(void)
+{
+	/* Enable GPIOD clock. */
+	/* Manually: */
+	// RCC_AHB1ENR |= RCC_AHB1ENR_IOPDEN;
+	/* Using API functions: */
+	rcc_periph_clock_enable(RCC_GPIOD);
+
+	/* Set GPIO12 (in GPIO port D) to 'output push-pull'. */
+	/* Manually: */
+	// GPIOD_CRH = (GPIO_CNF_OUTPUT_PUSHPULL << (((8 - 8) * 4) + 2));
+	// GPIOD_CRH |= (GPIO_MODE_OUTPUT_2_MHZ << ((8 - 8) * 4));
+	/* Using API functions: */
+	gpio_mode_setup(GPIOD, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO12);
+}
+
+int main(void) {
+
+  int i;
+
+  gpio_setup();
+
+    // Since our LED is on GPIO bank A, we need to enable
+    // the peripheral clock to this GPIO bank in order to use it.
+    // rcc_periph_clock_enable(RCC_GPIOA);
+
+    // Our test LED is connected to Port A pin 11, so let's set it as output
+    // gpio_mode_setup(GPIOA, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO11);
+
+    // Now, let's forever toggle this LED back and forth
+    while (true) {
+        gpio_toggle(GPIOD, GPIO12);
+        for (i = 0; i < 1000000; i++) {
+          __asm__("nop");
+        }
+    }
+
+    return 0;
+}

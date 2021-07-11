ee4d7d469f9effd4ac8ab106743f7184810aa723
Kyle Stewart <kylestew@gmail.com>
2021-07-11 04:36:49 -0600

    Add support for language server, vim development

--- main.c
+++ main.c
@@ -1,27 +1,25 @@
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/rcc.h>
 
-static void gpio_setup(void)
-{
-	/* Enable GPIOD clock. */
-	/* Manually: */
-	// RCC_AHB1ENR |= RCC_AHB1ENR_IOPDEN;
-	/* Using API functions: */
-	rcc_periph_clock_enable(RCC_GPIOD);
+static void gpio_setup(void) {
+    /* Enable GPIOD clock. */
+    /* Manually: */
+    // RCC_AHB1ENR |= RCC_AHB1ENR_IOPDEN;
+    /* Using API functions: */
+    rcc_periph_clock_enable(RCC_GPIOD);
 
-	/* Set GPIO12 (in GPIO port D) to 'output push-pull'. */
-	/* Manually: */
-	// GPIOD_CRH = (GPIO_CNF_OUTPUT_PUSHPULL << (((8 - 8) * 4) + 2));
-	// GPIOD_CRH |= (GPIO_MODE_OUTPUT_2_MHZ << ((8 - 8) * 4));
-	/* Using API functions: */
-	gpio_mode_setup(GPIOD, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO12);
+    /* Set GPIO12 (in GPIO port D) to 'output push-pull'. */
+    /* Manually: */
+    // GPIOD_CRH = (GPIO_CNF_OUTPUT_PUSHPULL << (((8 - 8) * 4) + 2));
+    // GPIOD_CRH |= (GPIO_MODE_OUTPUT_2_MHZ << ((8 - 8) * 4));
+    /* Using API functions: */
+    gpio_mode_setup(GPIOD, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO12);
 }
 
 int main(void) {
+    int i;
 
-  int i;
-
-  gpio_setup();
+    gpio_setup();
 
     // Since our LED is on GPIO bank A, we need to enable
     // the peripheral clock to this GPIO bank in order to use it.
@@ -31,10 +29,10 @@ int main(void) {
     // gpio_mode_setup(GPIOA, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO11);
 
     // Now, let's forever toggle this LED back and forth
-    while (true) {
+    while (1) {
         gpio_toggle(GPIOD, GPIO12);
         for (i = 0; i < 100000; i++) {
-          __asm__("nop");
+            __asm__("nop");
         }
     }
 
